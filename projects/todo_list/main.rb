f1e9a9318d584d38f931baf7ae8711ee1db2c709
Brahayan Xavier <bxsr@proton.me>
2025-06-23 11:12:21 -0500

    feat: add TODO List

---
+++ projects/todo_list/main.rb
@@ -0,0 +1,57 @@
+$tasks = []
+
+$options = <<~TEXT
+  ==========================================================
+  TODO LIST
+  ---------
+  Pick one of the following options (enter the number):
+
+  1. View tasks.
+  2. Create a task.
+  3. Remove a task.
+  ==========================================================
+TEXT
+
+def create_task
+  puts 'Describe the task to create:'
+  description = gets.chomp
+
+  $tasks << description
+end
+
+def remove_task
+  puts 'Select the task to remove:'
+  index = gets.chomp.to_i
+
+  $tasks.delete_at(index)
+end
+
+def view_tasks
+  puts 'Tasks:'
+  $tasks.each_with_index { |task, i| puts "#{i}. #{task}" }
+end
+
+def select_flow(option)
+  case option
+  when 1
+    view_tasks
+  when 2
+    create_task
+  when 3
+    remove_task
+  else
+    puts 'Invalid option.'
+  end
+end
+
+def main
+  loop do
+    print $options
+    option = gets.chomp.to_i
+
+    print '--------- '
+    select_flow option
+  end
+end
+
+main
