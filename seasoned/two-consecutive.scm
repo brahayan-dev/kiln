01c1162601026abda413f456c3c59bc5faf8f6ed
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2026-01-20 12:45:23 -0500

    feat: another case

--- seasoned/two-consecutive.scm
+++ seasoned/two-consecutive.scm
@@ -9,6 +9,19 @@
       (or (->checked (car coll) (cdr coll))
           (two-consecutive? (cdr coll)))))
 
-(two-consecutive? '(a b c d)) ;; #f
-(two-consecutive? '(a b a d)) ;; #f
-(two-consecutive? '(b a a d)) ;; #t
+(two-consecutive? '(a b c d)) ;; => #f
+(two-consecutive? '(a b a d)) ;; => #f
+(two-consecutive? '(b a a d)) ;; => #t
+
+(define (two-in-a-row-rec? head tail)
+  (if (null? tail) #f
+      (or (eq? head (car tail))
+          (two-in-a-row-rec? (car tail) (cdr tail)))))
+
+(define (two-in-a-row? coll)
+  (if (null? coll) #f
+      (two-in-a-row-rec? (car coll) (cdr coll))))
+
+(two-in-a-row? '(a b c d)) ;; => #f
+(two-in-a-row? '(a b a d)) ;; => #f
+(two-in-a-row? '(b a a d)) ;; => #t
