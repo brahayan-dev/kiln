178b7bb0370fe0d36e65eef991590670e28415cf
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2026-08-26 09:43:57 -0500

    feat: add basic impl. for Permutations

--- src/main/scala/kiln/math/Permutation.scala
+++ src/main/scala/kiln/math/Permutation.scala
@@ -2,10 +2,13 @@ package kiln.math
 
 import scala.collection.mutable.ArrayBuffer
 
-case class Permutation(n: Int):
-  require(n >= 0, "`n` should be a positive integer")
+case class Permutation(n: BigInt):
+  require(n > 0, "`n` should be a positive integer")
 
   private val cache = ArrayBuffer.empty[BigInt]
 
+  private def extend(n: BigInt, acc: BigInt) =
+    if n = 0 then acc else extend(n - 1, acc * n)
+
   def count: BigInt =
-    n
+    if n = 0 then 0 else extend(n, 1)
