61c40ece071bb4a866dbb721d20f229883000a54
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2026-08-28 13:06:22 -0500

    test: add rec tag

--- src/main/scala/kiln/math/Permutation.scala
+++ src/main/scala/kiln/math/Permutation.scala
@@ -1,18 +1,20 @@
 package kiln.math
 
 import scala.collection.mutable.ArrayBuffer
+import scala.annotation.tailrec
 
 case class Permutation(n: Int):
   require(n >= 0, "`n` should be a positive integer")
 
   private val cache = ArrayBuffer(1, 1)
 
+  @tailrec
   private def extend(k: Int): Int =
-    if k > n then
-      cache(n)
+    if k > n then cache(n)
     else
       cache.append(cache(k - 1) * k)
       extend(k + 1)
 
   def count: Int =
-    if n < cache.length then cache(n) else extend(cache.length)
+    val k = cache.length
+    if n < k then cache(n) else extend(k)
