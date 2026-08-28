021fe3305f87527bb6ea5ec0489234905252cb1f
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2026-08-28 12:10:21 -0500

    part: add cache

--- src/main/scala/kiln/math/Permutation.scala
+++ src/main/scala/kiln/math/Permutation.scala
@@ -5,10 +5,15 @@ import scala.collection.mutable.ArrayBuffer
 case class Permutation(n: BigInt):
   require(n >= 0, "`n` should be a positive integer")
 
-  private val cache = ArrayBuffer.empty[BigInt]
+  private val cache = ArrayBuffer(1, 1)
 
   private def extend(n: BigInt, acc: BigInt): BigInt =
-    if n == 0 then acc else extend(n - 1, acc * n)
+    if n == 0 then
+      acc
+    else
+      val newAcc = acc * n
+      cache.append(newAcc)
+      extend(n - 1, newAcc)
 
   def count: BigInt =
-    if n == 0 then 1 else extend(n, 1)
+    if n == 0 then cache(n) else extend(n, 1)
