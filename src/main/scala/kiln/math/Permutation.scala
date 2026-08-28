3c3286547f24cebe2860b64cc7c6ac46ecf99bb9
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2026-08-28 12:52:05 -0500

    part: check changes!

--- src/main/scala/kiln/math/Permutation.scala
+++ src/main/scala/kiln/math/Permutation.scala
@@ -2,18 +2,18 @@ package kiln.math
 
 import scala.collection.mutable.ArrayBuffer
 
-case class Permutation(n: BigInt):
+case class Permutation(n: Int):
   require(n >= 0, "`n` should be a positive integer")
 
   private val cache = ArrayBuffer(1, 1)
 
-  private def extend(n: BigInt, acc: BigInt): BigInt =
-    if n == 0 then
-      acc
-    else
+  private def extend(n: Int, acc: Int): Int =
+    if cache.length < n then
       val newAcc = acc * n
       cache.append(newAcc)
       extend(n - 1, newAcc)
+    else
+      cache(this.n)
 
-  def count: BigInt =
-    if n == 0 then cache(n) else extend(n, 1)
+  def count: Int =
+    if n <= 1 then cache(n) else extend(n, 1)
